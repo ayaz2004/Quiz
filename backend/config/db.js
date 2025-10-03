58686b6c29b60c0c0a73290506d92a3cb40add92
mohdhammadqadir <hammad.qadir@educase.io>
2025-10-04 00:04:44 +0530

    Refactor code structure for improved readability and maintainability

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+import {Client} from 'pg';
+
+const conn = new Client({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+
+})
+
+conn.connect().then(()=>console.log("dbconnection setup complete")).catch(e=>console.log(e));
---
+++ backend/db/index.js
@@ -0,0 +1,1 @@
+// all the db queries will be here
