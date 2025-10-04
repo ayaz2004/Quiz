1452b80a1f9cf5b8f65ee6254f7945a0a4ea1456
ayaz2004 <nazirayaz03@gmail.com>
2025-10-04 15:42:57 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import pool from './config/db.js';
+import pool from './config/db.comfig.js';
 
 dotenv.config();
 
