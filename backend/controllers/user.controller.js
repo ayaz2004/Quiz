e4c5457d0d6f7d74b0b8b8cfa833a96196b2562a
ayaz2004 <ayaznazir03@gmail.com>
2026-02-02 13:01:30 +0530

    Switch to SMTP port 465 SSL for email

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -9,15 +9,17 @@ import crypto from 'crypto';
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true, // Use SSL
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
   tls: {
     rejectUnauthorized: false
-  }
+  },
+  connectionTimeout: 10000, // 10 seconds
+  greetingTimeout: 10000
 });
 
 export const createUser = async (req, res, next) => {
